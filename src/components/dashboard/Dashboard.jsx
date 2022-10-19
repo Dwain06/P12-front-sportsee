796562fde90f69a4a39c0e01c1ffa32518338d63
Dwain06 <dwain.mobile@gmail.com>
2022-10-19 15:23:54 +0200

    add(nutrients): add nutrients component

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import GetUserData from '../../data/GetUserData';
+import ActivityCharts from '../charts/ActivityCharts';
+import Nutrients from '../nutrients/Nutrients';
 
 const Dashboard = () => {
-    
-    // const [isLoaded, setIsLoaded] = useState(false);
+
     const [generalInformations, setGeneralInformations] = useState();
-    const [activity, setactivity] = useState();    
-    const [averageSessions, setAverageSessions] = useState();    
-    const [performance, setPerformance] = useState();    
-    
+    const [nutrients, setNutrients] = useState();
+    const [activity, setactivity] = useState();
+    const [averageSessions, setAverageSessions] = useState();
+    const [performance, setPerformance] = useState();
+
     const userId = 12;
-    
+
     useEffect(() => {
         async function getDatas() {
             const userData = new GetUserData(userId);
             setGeneralInformations(await userData.generalInformations());
+            setNutrients(await userData.nutrients());
             setactivity(await userData.activity());
             setAverageSessions(await userData.averageSessions());
             setPerformance(await userData.performance());
         }
         getDatas();
 
-        
+
     }, []);
-    
-    
-    
+
+
     return (
         <>
             {generalInformations && activity && averageSessions && performance ?
                 <div className='dashboard'>
-                    <div className="welcome">
+                    <div className="dashboard__welcome">
                         <h2>
                             Bonjour <span className='name'>{generalInformations.userInfos.firstName}</span>
                         </h2>
@@ -42,6 +44,22 @@ const Dashboard = () => {
                             {averageSessions.userId}
                             {performance.userId}
                         </p>
+                    </div>
+                    <div className="dashboard__datas">
+                        <div className="datas__charts">
+                            <div className="datas__charts--activity">
+                                <ActivityCharts props={activity.sessions}/>
+                            </div>
+                            <div className="datas__charts--scores">
+
+                            </div>
+                        </div>
+                        <div className="datas--nutrients">
+                            {nutrients.map((nutrient, index) => (
+                                <Nutrients key={index + 1} props={nutrient} />
+                            ))}
+
+                        </div>
                     </div>
                 </div>
                 :
--- src/data/GetUserData.js
+++ src/data/GetUserData.js
@@ -13,6 +13,32 @@ class GetUserData {
         return generalInformations.data;
     }
 
+    async nutrients(){
+        const nutrients = await fetchData(domainName + "user/" + this.id);
+        return [
+            {
+                name: "Calories",
+                count: (nutrients.data.keyData.calorieCount / 1000).toFixed(3) + "kCal",
+                img: "calorieCount"
+            },
+            {
+                name: "Proteines",
+                count: nutrients.data.keyData.proteinCount + "g",
+                img: "proteinCount"
+            },
+            {
+                name: "Glucides",
+                count: nutrients.data.keyData.carbohydrateCount + "g",
+                img: "carbohydrateCount"
+            },
+            {
+                name: "Lipides",
+                count: nutrients.data.keyData.lipidCount + "g",
+                img: "lipidCount"
+            }
+    ]
+    }
+
     async activity() {
         const activity = await fetchData(domainName + "user/" + this.id + "/activity");
         return activity.data;
